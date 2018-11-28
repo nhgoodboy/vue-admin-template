59ebd65274f78b1b92ef44cf9020f92e431af54b
nhgoodboy <evan.yjz@gmail.com>
2018-11-28 10:21:58 +0800

    yjz dev.env & prod.env 修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,8 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8081"',
-  UPLOAD_URL: '"http://localhost:8081/admin/fileInf/upload"'
+  // BASE_API: '"http://localhost:8081"',
+  BASE_API: '"http://104.199.198.38:8082"',
+  // UPLOAD_URL: '"http://localhost:8081/admin/fileInf/upload"'
+  UPLOAD_URL: '"http://104.199.198.38:8082/admin/fileInf/upload"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  BASE_API: '"http://127.0.0.1:8082"',
+  UPLOAD_URL: '"http://127.0.0.1:8082/admin/fileInf/upload"'
 }
