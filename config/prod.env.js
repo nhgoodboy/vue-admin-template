256571a16f73ceb98ba4bfc2537fce3d8ff823f4
nhgoodboy <evan.yjz@gmail.com>
2019-02-01 14:15:51 +0800

    yjz 变更服务端地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://127.0.0.1:8082"',
-  UPLOAD_URL: '"http://127.0.0.1:8082/admin/fileInf/upload"'
+  BASE_API: '"http://35.220.190.136:8082"',
+  UPLOAD_URL: '"http://35.220.190.136:8082/admin/fileInf/upload"'
 }
